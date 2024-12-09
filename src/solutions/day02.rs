afd36b2d9e66db8e8c5cc7263272bab461a38150
Eiriksak <eiriksak@hotmail.com>
2024-12-09 20:15:20 +0100

    d2 p2

--- src/solutions/day02.rs
+++ src/solutions/day02.rs
@@ -18,6 +18,7 @@ fn parse_data() -> Vec<Box<[i16]>> {
 fn check_safe(row: &[i16], max_diff: i16) -> i16 {
     let is_increasing = row[0] < row[1]; // determine trend
 
+    // .all to stop immediate as it fails
     if row.windows(2).all(|pair| {
         let diff = pair[0] - pair[1];
         if is_increasing {
@@ -32,10 +33,36 @@ fn check_safe(row: &[i16], max_diff: i16) -> i16 {
     }
 }
 
+fn check_safe2(row: &[i16], max_diff: i16) -> i16 {
+    let is_increasing = row[0] < row[1]; // determine trend
+
+    // .filter to loop all and .count to sum up fails
+    let n_fails = row
+        .windows(2)
+        .filter(|pair| {
+            let diff = pair[0] - pair[1];
+            if is_increasing {
+                !(diff < 0 && diff.abs() <= max_diff)
+            } else {
+                !(diff > 0 && diff <= max_diff)
+            }
+        })
+        .count();
+
+    if n_fails > 1 {
+        0
+    } else {
+        1
+    }
+}
+
 pub fn main() {
     let data = parse_data();
     //println!("{:#?}", data);
 
-    let results: i16 = data.iter().map(|row| check_safe(row, 3)).sum();
-    println!("{:#?}", results);
+    let res1: i16 = data.iter().map(|row| check_safe(row, 3)).sum();
+    println!("Part 1 {:#?}", res1);
+
+    let res2: i16 = data.iter().map(|row| check_safe2(row, 3)).sum();
+    println!("Part 2 {:#?}", res2);
 }
